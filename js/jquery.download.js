f15a1c2c736b4f51538c8cb7e5229036b9c95ae0
JungHyunKwon <kjh_3859@naver.com>
2019-02-26 13:42:15 +0900

    published

---
+++ js/jquery.download.js
@@ -0,0 +1,48 @@
+/**
+ * @author JungHyunKwon
+ * @version 1.0.0
+ */
+try {
+	'use strict';
+
+	(function($) {
+		//제이쿼리가 함수일 때
+		if(typeof $ === 'function') {
+			var _toString = Object.prototype.toString;
+
+			/**
+			 * @name download
+			 * @since 2017-12-06
+			 * @param {array} attributes
+			 * @return {jQuery}
+			 */
+			$.fn.download = function(attributes) {
+				var result = this;
+				
+				//배열일 때
+				if(_toString.call(attributes) === '[object Array]') {
+					var attributesLength = attributes.length;
+
+					this.each(function(index, element) {
+						var $element = $(element);
+
+						for(var i = 0; i < attributesLength; i++) {
+							var attribute = $element.attr(attributes[i]);
+							
+							//속성이 있을 때
+							if(attribute) {
+								open(attribute, '_blank');
+							}
+						}
+					});
+				}
+
+				return result;
+			};
+		}else{
+			throw '제이쿼리가 없습니다.';
+		}
+	})(window.jQuery);
+}catch(e) {
+	console.error(e);
+}
