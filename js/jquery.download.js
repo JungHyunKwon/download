4cd37a9d8af5b93fd830ca248530794af1be6188
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:09:18 +0900

    published

--- js/jquery.download.js
+++ js/jquery.download.js
@@ -2,47 +2,38 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
+(function($) {
 	'use strict';
 
-	(function($) {
-		//제이쿼리가 함수일 때
-		if(typeof $ === 'function') {
-			var _$isArray = $.isArray;
+	var _$isArray = $.isArray;
 
-			/**
-			 * @name download
-			 * @since 2017-12-06
-			 * @param {array} attributes
-			 * @return {jQuery}
-			 */
-			$.fn.download = function(attributes) {
-				var result = this;
+	/**
+	 * @name download
+	 * @since 2017-12-06
+	 * @param {array} attributes
+	 * @return {object}
+	 */
+	$.fn.download = function(attributes) {
+		var result = this;
+		
+		//배열일 때
+		if(_$isArray(attributes)) {
+			var attributesLength = attributes.length;
+
+			this.each(function(index, element) {
+				var $element = $(element);
 				
-				//배열일 때
-				if(_$isArray(attributes)) {
-					var attributesLength = attributes.length;
-
-					this.each(function(index, element) {
-						var $element = $(element);
-						
-						for(var i = 0; i < attributesLength; i++) {
-							var attribute = $element.attr(attributes[i]);
-							
-							//속성이 있을 때
-							if(attribute) {
-								open(attribute, '_blank');
-							}
-						}
-					});
+				for(var i = 0; i < attributesLength; i++) {
+					var attribute = $element.attr(attributes[i]);
+					
+					//속성이 있을 때
+					if(attribute) {
+						open(attribute, '_blank');
+					}
 				}
-
-				return result;
-			};
-		}else{
-			throw '제이쿼리가 없습니다.';
+			});
 		}
-	})(window.jQuery);
-}catch(e) {
-	console.error(e);
-}
+
+		return result;
+	};
+})(jQuery);
